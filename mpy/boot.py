d5f0acbd7714016b7a21546831b06b855340a15d
xzqiaochu <xzzhangqiaochu@163.com>
2022-05-22 14:02:14 +0800

    【Update】不发送空白帧&id保存在文件中

--- mpy/boot.py
+++ mpy/boot.py
@@ -1,29 +1,32 @@
 from Maix import MIC_ARRAY as mic
 # import lcd
 
-ID = 0x01
+f = open('id.txt', 'r')
+ID = int(f.read())
+f.close()
 
 mic.init()
-brightness = [0 for _ in range(12)]
-mic.set_led(brightness, (0, 0, 0))
+mic.set_led([0 for _ in range(12)], (0, 0, 0))
 # lcd.init()
 
 while True:
     imga = mic.get_map()
     msg = [0xA5, ID]
     s = 0
+    valid = False
     for i in range(16 * 16):
         data = imga[i]
+        if data != 0:
+            valid = True
         msg.append(data)
         s = s + data
     msg.append(s % 0x100)
     msg.append(0x5A)
-    #print(len(msg))
-    for data in msg:
-        print("%c" % data, end = "")
+    if valid:
+        for data in msg:
+            print("%c" % data, end = "")
 
-    brightness = mic.get_dir(imga)
-    mic.set_led(brightness, (255, 0, 0))
+    mic.set_led(mic.get_dir(imga), (255, 0, 0))
 
     # imgb = imga.resize(160,160)
     # imgc = imgb.to_rainbow(1)
