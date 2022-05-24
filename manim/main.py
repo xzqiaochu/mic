bf0454118d7bfef1a31e01260186b76a782279f3
xzqiaochu <xzzhangqiaochu@163.com>
2022-05-25 00:24:08 +0800

    【Update】完成一点点动画

--- manim/main.py
+++ manim/main.py
@@ -19,5 +19,28 @@ class Scene1(Scene):
         self.play(ApplyMethod(text2.shift, UP), FadeIn(text3, UP))
         # self.wait()
 
+class Scene2(Scene):
+    def construct(self):
+        image_person = ImageMobject("./manim/img/person.png", height = 3)
+        image_person.move_to(2 * DOWN)
+        self.play(SpinInFromNothing(image_person))
+
+        image_car = ImageMobject("./manim/img/car.gif", height = 2)
+        image_car.move_to(2 * UP + 4 * RIGHT)
+        self.play(SpinInFromNothing(image_car))
+
+        line1 = Line(2.4 * UP + 4 * RIGHT, 2 * DOWN + 0.8 * RIGHT, stroke_color = BLUE)
+        line2 = Line(2.4 * UP + 4 * RIGHT, 2 * DOWN + 0.8 * LEFT, stroke_color = BLUE)
+        self.play(ShowCreation(line1), ShowCreation(line2))
+
+        line3 = Line(2.4 * UP + 4 * LEFT, 2 * DOWN + 0.8 * RIGHT, stroke_color = BLUE)
+        line4 = Line(2.4 * UP + 4 * LEFT, 2 * DOWN + 0.8 * LEFT, stroke_color = BLUE)
+        self.play(ApplyMethod(image_car.shift, 8 * LEFT),
+                ReplacementTransform(line1, line3),
+                ReplacementTransform(line2, line4),
+                rate_func = smooth, run_time = 3)
+
 if __name__ == "__main__":
-    system("manimgl %s Scene1" % __file__)
+    system("manimgl %s Scene2" % __file__)
+
+# manimgl manim/main.py -w --uhd
