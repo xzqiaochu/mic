d8223d3ea4879e6a78016624fec5ced83144e4d7
xzqiaochu <xzzhangqiaochu@163.com>
2022-05-25 20:35:49 +0800

    【Update】完成Scene2动画

--- manim/main.py
+++ manim/main.py
@@ -21,11 +21,11 @@ class Scene1(Scene):
 
 class Scene2(Scene):
     def construct(self):
-        image_person = ImageMobject("./manim/img/person.png", height = 3)
+        image_person = ImageMobject("./manim/img/person", height = 3)
         image_person.move_to(2 * DOWN)
         self.play(SpinInFromNothing(image_person))
 
-        image_car = ImageMobject("./manim/img/car.gif", height = 2)
+        image_car = ImageMobject("./manim/img/car", height = 2)
         image_car.move_to(2 * UP + 4 * RIGHT)
         self.play(SpinInFromNothing(image_car))
 
@@ -39,6 +39,40 @@ class Scene2(Scene):
                 ReplacementTransform(line1, line3),
                 ReplacementTransform(line2, line4),
                 rate_func = smooth, run_time = 3)
+
+        self.play(Indicate(line4))
+        self.play(FadeOut(line4))
+
+        line5 = Line(2.4 * UP + 4 * RIGHT, 2 * DOWN + 0.8 * RIGHT, stroke_color = BLUE)
+        image_question = ImageMobject("./manim/img/question", height = 1)
+        image_question.move_to(1 * DOWN + 1 * RIGHT)
+        self.play(ApplyMethod(image_car.shift, 8 * RIGHT),
+                ReplacementTransform(line3, line5),
+                SpinInFromNothing(image_question),
+                rate_func = smooth, run_time = 3)
+
+        line6 = Line(2.4 * UP + 4 * RIGHT, 2 * DOWN + 0.8 * LEFT, stroke_color = BLUE)
+        self.add(line6)
+        self.remove(image_question)
+
+        b1 = Brace(line5, direction = line5.copy().rotate(PI / 2).get_unit_vector())
+        b1text = b1.get_text("L", font="SimSun")
+        line5_len = np.linalg.norm((2.4 * UP + 4 * RIGHT) - (2 * DOWN + 0.8 * RIGHT))
+        line6_dir = (2 * DOWN + 0.8 * LEFT) - (2.4 * UP + 4 * RIGHT)
+        line6_dir = line6_dir / np.linalg.norm(line6_dir)
+        line6s1_start = 2.4 * UP + 4 * RIGHT
+        line6s1_end = line6s1_start + np.array([_ * line5_len for _ in line6_dir])
+        line6s1 = Line(line6s1_start, line6s1_end)
+        b2 = Brace(line6s1, direction = line6s1.copy().rotate(- PI / 2).get_unit_vector())
+        b2text = b2.get_text("L", font="SimSun")
+        line6s2_start = line6s1_end
+        line6s2_end = 2 * DOWN + 0.8 * LEFT
+        line6s2 = Line(line6s2_start, line6s2_end)
+        b3 = Brace(line6s2, direction = line6s2.copy().rotate(- PI / 2).get_unit_vector())
+        b3text = b3.get_text("△L", font="SimSun")
+        self.play(ShowCreation(b1), ShowCreation(b1text),
+                ShowCreation(b2), ShowCreation(b2text),
+                ShowCreation(b3), ShowCreation(b3text))
 
 if __name__ == "__main__":
     system("manimgl %s Scene2" % __file__)
